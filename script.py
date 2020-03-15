9815f415d4caba2fb0843b3e6d0bd28e68db88bb
chirag2506 <chiraggupta2506@gmail.com>
2020-03-15 19:57:14 +0530

    Updated script.py

--- script.py
+++ script.py
@@ -1,3 +1,16 @@
 #code
 
-import pypdf2
+import PyPDF2
+from PyPDF2 import PdfFileReader
+
+def get_info(path):
+
+    with open(path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        info = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+        print(info)
+
+path = 'sample.pdf'
+get_info(path)
